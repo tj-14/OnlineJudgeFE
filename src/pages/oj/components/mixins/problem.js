ce9a112b8370396116b92d215abe8abbeff87390
MW-7892 <sk.14055450@gmail.com>
2022-04-03 01:37:26 +0700

    added score column display on FE

--- src/pages/oj/components/mixins/problem.js
+++ src/pages/oj/components/mixins/problem.js
@@ -3,7 +3,8 @@ import utils from '@/utils/utils'
 export default {
   data () {
     return {
-      statusColumn: false
+      statusColumn: false,
+      scoreColumn: false
     }
   },
   methods: {
@@ -42,6 +43,72 @@ export default {
         }
       })
       this.statusColumn = true
+    },
+    addScoreColumn (tableColumns, dataProblems) {
+      if (this.scoreColumn) return
+      let needAdd = dataProblems.some((item, index) => {
+        if (item.my_score !== null && item.my_score !== undefined) {
+          return true
+        }
+      })
+      if (!needAdd) {
+        return
+      }
+      tableColumns.splice(4, 0, {
+        title: 'Score',
+        'min-width': 100,
+        'max-width': 200,
+        render: (h, params) => {
+          let score = params.row.my_score
+          let totalScore = params.row.total_score
+          if (score === null || score === undefined) {
+            return undefined
+          }
+          let scoreColor = '#19be6b'
+          if (score < 100 && score > 0) {
+            scoreColor = '#ffc233'
+          } else if (score === 0) {
+            scoreColor = '#ed3f14'
+          }
+          let progressBar = h('div', {
+            style: {
+              'background-color': scoreColor,
+              'border-radius': '10px',
+              position: 'absolute',
+              'z-index': 2,
+              width: (score * 100 / totalScore).toString() + '%',
+              'min-height': '100%'
+            }
+          })
+          let progressBackground = h('div', {
+            style: {
+              'background-color': 'red',
+              'border-radius': '10px',
+              position: 'absolute',
+              width: '100%',
+              'min-height': '100%'
+            }
+          })
+          let scoreDisplay = h('div', {
+            style: {
+              textAlign: 'center',
+              color: 'white',
+              'font-weight': 'bold',
+              position: 'absolute',
+              'z-index': 3,
+              width: '100%'
+            }
+          }, score.toString() + ' / ' + totalScore.toString())
+          return h('div', {
+            style: {
+              height: '20px',
+              position: 'relative',
+              width: '100%'
+            }
+          }, [progressBar, progressBackground, scoreDisplay])
+        }
+      })
+      this.scoreColumn = true
     }
   }
 }
